1a40000a22463e318cc65edfb87ab226c985ceec
Sebastian Suarez <ssuarez@yellowpepper.com>
2022-05-22 08:42:12 -0500

    :recycle: Update locators to clearer

--- src/main/java/mobile/e2e/appium/ui/CategoryPage.java
+++ src/main/java/mobile/e2e/appium/ui/CategoryPage.java
@@ -12,7 +12,7 @@ public class CategoryPage {
     public static final Target TXT_NAME_CATEGORY = Target.the("Text box to add the name of the new category").located(By.id("editCategoryName"));
     public static final Target TXT_ENTER_COLOR = Target.the("Text box to enter the color category").located(By.id("cpv_hex"));
     public static final Target IMG_CATEGORY = Target.the("Image to select the icon of the new category").locatedBy("//android.widget.RelativeLayout[{0}]");
-    public static final Target BTN_CONFIRM_CATEGORY = Target.the("Button to confirm the new category").locatedBy("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageButton");
+    public static final Target BTN_CONFIRM_CATEGORY = Target.the("Button to confirm the new category").located(By.id("confirm"));
     public static final Target BTN_SEARCH_CATEGORY = Target.the("Button to search a category").located(By.id("menuItem_search"));
     public static final Target TXT_SEARCH_CATEGORY = Target.the("Text box to search a category").located(By.id("search_src_text"));
 
